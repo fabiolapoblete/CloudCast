7ec1c07e744bd667caee4d5f01f81c0b41b3bf67
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-02-06 21:22:06 +0100

    Added map, additional styling and eventlistener on search

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ let visibility = document.querySelector(".visibility_value");
 let visibilityIcon = document.querySelector(".icon_visibility");
 let detailsHeader = document.querySelector(".details__header");
 
+let city = "Stockholm";
+const input = document.querySelector(".search__bar");
+
+getWeather(city);
+
+input.addEventListener("search", (e) => {
+  e.preventDefault();
+  city = input.value;
+  console.log(city);
+  getWeather(city);
+});
+
 async function getWeather(city) {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -25,18 +37,23 @@ async function getWeather(city) {
     <hr>
     `;
 
+    let lon = weatherData.coord.lon;
+    let lat = weatherData.coord.lat;
+    console.log(lon, lat);
+
     displayWeather(weatherData);
+    displayMap(lat, lon);
   } catch (error) {
     console.log(error);
   }
 }
 
 function displayWeather(weatherData) {
-  temperatureMax.innerText = weatherData.main.temp_max + " °C";
+  temperatureMax.innerText = Math.round(weatherData.main.temp_max) + " °C";
 
-  temperatureMin.innerText = weatherData.main.temp_min + " °C";
+  temperatureMin.innerText = Math.round(weatherData.main.temp_min) + " °C";
 
-  wind.innerText = weatherData.wind.speed + " m/s";
+  wind.innerText = Math.round(weatherData.wind.speed) + " m/s";
 
   humidity.innerText = weatherData.main.humidity + " %";
 
@@ -50,4 +67,14 @@ function displayWeather(weatherData) {
     "@2x.png";
 }
 
-getWeather("London");
+function displayMap(lat, lon) {
+  var map = L.map("map").setView([lat, lon], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  var marker = L.marker([lat, lon]).addTo(map);
+}
+
+// getWeather("Stockholm");
